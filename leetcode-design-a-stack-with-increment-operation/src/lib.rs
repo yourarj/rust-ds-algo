1267c716fd93702408d1fb36f7e3ec9befa3e2c4
yourarj <11359226+yourarj@users.noreply.github.com>
2022-08-04 07:42:25 +0530

    rust solution to design a stack with increment operation
     On branch main
    
     Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	new file:   leetcode-design-a-stack-with-increment-operation/Cargo.toml
    	new file:   leetcode-design-a-stack-with-increment-operation/README.md
    	new file:   leetcode-design-a-stack-with-increment-operation/src/lib.rs

---
+++ leetcode-design-a-stack-with-increment-operation/src/lib.rs
@@ -0,0 +1,75 @@
+pub struct CustomStack {
+    max_size: i32,
+    inner: Vec<i32>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl CustomStack {
+    pub fn new(max_size: i32) -> Self {
+        CustomStack {
+            max_size,
+            inner: Vec::with_capacity(max_size as usize),
+        }
+    }
+
+    pub fn push(&mut self, x: i32) {
+        println!(
+            "Push {:02} : Current stack length is {}",
+            x,
+            self.inner.len()
+        );
+        if self.inner.len() < self.max_size as usize {
+            self.inner.push(x);
+            println!("{:?}", self.inner);
+        }
+    }
+
+    pub fn pop(&mut self) -> i32 {
+        match self.inner.pop() {
+            Some(num) => num,
+            None => -1,
+        }
+    }
+
+    pub fn increment(&mut self, k: i32, val: i32) {
+        for (index, elem) in self.inner.iter_mut().enumerate() {
+            if index < k as usize {
+                *elem += val;
+            }
+        }
+    }
+}
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * let obj = CustomStack::new(maxSize);
+ * obj.push(x);
+ * let ret_2: i32 = obj.pop();
+ * obj.increment(k, val);
+ */
+
+#[cfg(test)]
+mod tests {
+    use super::CustomStack;
+    #[test]
+    fn test_01() {
+        // ["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+        // [[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+        let mut obj = CustomStack::new(3);
+        obj.push(1);
+        obj.push(2);
+        assert_eq!(obj.pop(), 2);
+        obj.push(2);
+        obj.push(3);
+        obj.push(4);
+        obj.increment(5, 100);
+        obj.increment(2, 100);
+        assert_eq!(obj.pop(), 103);
+        assert_eq!(obj.pop(), 202);
+        assert_eq!(obj.pop(), 201);
+        assert_eq!(obj.pop(), -1);
+    }
+}
