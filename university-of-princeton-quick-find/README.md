fbb5a8b68e312a63085fa9933a179af7db0aece1
yourarj <11359226+yourarj@users.noreply.github.com>
2022-12-24 09:01:29 +0000

    unit tests and readme update
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sat Dec 24 09:01:29 2022 +0000
    
     On branch main
    
     Changes to be committed:
    	modified:   university-of-princeton-quick-find/README.md
    	modified:   university-of-princeton-quick-find/src/lib.rs

--- university-of-princeton-quick-find/README.md
+++ university-of-princeton-quick-find/README.md
@@ -3,3 +3,5 @@
 ## Part of Algorthm part I coursersa & University of Princeton
 
 Find if two elements are connected or not
+
+[Coursera | University of Princeton : Quick Find](https://www.coursera.org/learn/algorithms-part1/lecture/EcF3P/quick-find)
--- university-of-princeton-quick-find/src/lib.rs
+++ university-of-princeton-quick-find/src/lib.rs
@@ -28,5 +28,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn it_works() {
+        let mut arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut qf = QuickFind::new(&mut arr);
+        qf.union(4, 3);
+        qf.union(3, 8);
+        qf.union(6, 5);
+        qf.union(9, 4);
+        qf.union(2, 1);
+        qf.union(8, 9);
+        qf.union(5, 0);
+        qf.union(7, 2);
+        qf.union(6, 1);
+        qf.union(1, 0);
+        qf.union(6, 7);
+
+        assert_eq!(false, qf.connected(4, 6));
+        assert_eq!(true, qf.connected(3, 4));
+        assert_eq!(true, qf.connected(6, 2));
+        assert_eq!(true, qf.connected(6, 6));
+    }
 }
