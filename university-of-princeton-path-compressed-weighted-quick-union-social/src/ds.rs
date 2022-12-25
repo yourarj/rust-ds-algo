836db7c78e33c2c7320a0dc280c390cd294dd003
yourarj <11359226+yourarj@users.noreply.github.com>
2022-12-25 13:39:23 +0530

    social connections
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Dec 25 13:39:23 2022 +0530
    
     On branch main
    
     Changes to be committed:
    	new file:   university-of-princeton-path-compressed-weighted-quick-union-social/src/ds.rs
    	modified:   university-of-princeton-path-compressed-weighted-quick-union-social/src/lib.rs

---
+++ university-of-princeton-path-compressed-weighted-quick-union-social/src/ds.rs
@@ -0,0 +1,102 @@
+/// Weighted Quick union
+/// In this we would assume the index as the number itself and
+/// value at the index as the parent of the that number
+/// if a number points to itself means it's root of the tree.
+/// In addition to data structures in naive Quick Union we also
+/// maintain additional array representing weight of the specific ith node.
+
+pub struct WeightedQuickUnion<const SIZE: usize> {
+    arr: [usize; SIZE],
+    weights: [usize; SIZE],
+}
+
+impl<const SIZE: usize> WeightedQuickUnion<SIZE> {
+    /// create new instance of QuickUnion
+    pub fn new() -> Self {
+        let mut arr = [0; SIZE];
+
+        for index in 0..SIZE {
+            arr[index] = index;
+        }
+        WeightedQuickUnion {
+            arr: arr,
+            weights: [1; SIZE],
+        }
+    }
+
+    /// find the root of given num
+    fn root(&mut self, mut num: usize) -> usize {
+        while self.arr[num] != num {
+            self.arr[num] = self.arr[self.arr[num]];
+            num = self.arr[num];
+        }
+        num
+    }
+
+    /// check if specified numbers are connected
+    pub fn connected(&mut self, a: usize, b: usize) -> bool {
+        self.root(a) == self.root(b)
+    }
+
+    pub fn completely_connected(&mut self) -> bool {
+        let root = self.root(0);
+        self.weights.len() == self.weights[root]
+    }
+
+    pub fn union(&mut self, a: usize, b: usize) {
+        // TODO update code to for weight comparison
+        let root_a = self.root(a);
+        let root_b = self.root(b);
+
+        if root_a == root_b {
+            return;
+        }
+
+        if self.weights[root_a] < self.weights[root_b] {
+            self.arr[root_a] = root_b;
+            self.weights[root_b] += self.weights[root_a];
+        } else {
+            self.arr[root_b] = root_a;
+            self.weights[root_a] += self.weights[root_b];
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works_from_lecture() {
+        let mut qf = WeightedQuickUnion::<10>::new();
+        qf.union(4, 3);
+        qf.union(3, 8);
+        qf.union(6, 5);
+        qf.union(9, 4);
+        qf.union(2, 1);
+        qf.union(5, 0);
+        qf.union(7, 2);
+        qf.union(6, 1);
+        qf.union(7, 3);
+
+        // the above unions creates single connected structure
+        //
+        // array should be
+        // arr = {6, 2, 6, 4, 6, 6, 6, 2, 4, 4}
+        //
+        // and weights should be
+        // weights = {1, 1, 3, 1, 4, 1, 10, 1, 1, 1}
+
+        // set two all mus be connected
+        assert_eq!(true, qf.connected(0, 0));
+        assert_eq!(true, qf.connected(0, 1));
+        assert_eq!(true, qf.connected(0, 2));
+        assert_eq!(true, qf.connected(0, 3));
+        assert_eq!(true, qf.connected(0, 4));
+        assert_eq!(true, qf.connected(0, 5));
+        assert_eq!(true, qf.connected(0, 6));
+        assert_eq!(true, qf.connected(0, 7));
+        assert_eq!(true, qf.connected(0, 8));
+        assert_eq!(true, qf.connected(0, 9));
+    }
+}
--- university-of-princeton-path-compressed-weighted-quick-union-social/src/lib.rs
+++ university-of-princeton-path-compressed-weighted-quick-union-social/src/lib.rs
@@ -1,5 +1,28 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod ds;
+
+use ds::WeightedQuickUnion;
+
+pub struct Social<const SIZE: usize> {
+    uf: WeightedQuickUnion<SIZE>,
+}
+
+impl<const SIZE: usize> Social<SIZE> {
+    /// create new instance of Social
+    pub fn new() -> Self {
+        Social {
+            uf: WeightedQuickUnion::new(),
+        }
+    }
+
+    /// find the root of given num
+    pub fn befriend(&mut self, one: usize, two: usize) -> bool {
+        self.uf.union(one, two);
+        self.uf.completely_connected()
+    }
+
+    pub fn everybody_socialized(&mut self) -> bool {
+        self.uf.completely_connected()
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +30,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_works_from_lecture() {
+        let mut soc = Social::<10>::new();
+        soc.befriend(4, 3);
+        soc.befriend(3, 8);
+        soc.befriend(6, 5);
+        soc.befriend(9, 4);
+        soc.befriend(2, 1);
+        soc.befriend(5, 0);
+        soc.befriend(7, 2);
+        soc.befriend(6, 1);
+        soc.befriend(7, 3);
     }
 }
