636e7dbf19b6d69d41b5ed5b509c5eed7005fd75
Arjun <>
2023-01-09 08:00:42 +0530

    solution approach two
    Signed-off-by: Arjun <>
    
     Date:      Mon Jan 9 08:00:42 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   gg-minimize-the-heights/src/lib.rs

--- gg-minimize-the-heights/src/lib.rs
+++ gg-minimize-the-heights/src/lib.rs
@@ -1,20 +1,28 @@
-pub fn get_min_diff(arr: &mut [i32], _n: i32, k: i32) -> i32 {
-    println!("n: {:?}", _n);
-    println!("k: {:?}", k);
-    println!("arr: {:?}", arr);
-    for num in arr.iter_mut() {
-        if *num >= k {
-            *num -= k;
-        } else {
-            *num += k;
-        }
-    }
-    println!("{:?}", arr);
+use std::cmp::{max, min};
 
+pub fn get_min_diff(arr: &mut [i32], n: usize, k: i32) -> i32 {
+    // sorting the array
     arr.sort();
-    println!("{:?}", arr);
 
-    arr[arr.len() - 1] - arr[0]
+    // get min difference
+    let mut min_diff = arr[n - 1] - arr[0];
+
+    // finding initial minimum difference
+    let mut smallest_tower = arr[0] + k;
+    let mut largest_tower = arr[n - 1] - k;
+
+    for num in 0..n - 1 {
+        smallest_tower = min(smallest_tower, arr[num + 1] - k);
+        largest_tower = max(largest_tower, arr[num] + k);
+
+        if smallest_tower < 0 {
+            continue;
+        }
+
+        min_diff = min(min_diff, largest_tower - smallest_tower);
+    }
+
+    min_diff
 }
 
 #[cfg(test)]
@@ -29,6 +37,7 @@ mod tests {
         let result = get_min_diff(&mut arr, n, k);
         assert_eq!(result, 5);
     }
+
     #[test]
     fn test_02() {
         let k = 3;
@@ -45,6 +54,7 @@ mod tests {
         let result = get_min_diff(&mut arr, n, k);
         assert_eq!(result, 0);
     }
+
     #[test]
     fn test_04_max_case() {
         let k = 10000;
@@ -61,5 +71,15 @@ mod tests {
         let mut arr = [2, 6, 3, 4, 7, 2, 10, 3, 2, 1];
         let result = get_min_diff(&mut arr, n, k);
         assert_eq!(result, 7);
+    }
+
+    #[test]
+    fn test_06() {
+        let k = 3;
+        let n = 3;
+        let mut arr = [1, 3, 6];
+        let result = get_min_diff(&mut arr, n, k);
+        let expected = 3;
+        assert_eq!(result, expected);
     }
 }
