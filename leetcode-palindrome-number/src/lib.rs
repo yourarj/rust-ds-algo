547e141117ecfde960f06782cb3921d3324394cd
Arjun <>
2023-04-06 22:56:35 +0530

    palindrome number
    Signed-off-by: Arjun <>
    
     Date:      Thu Apr 6 22:56:35 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	new file:   leetcode-palindrome-number/Cargo.toml
    	new file:   leetcode-palindrome-number/README.md
    	new file:   leetcode-palindrome-number/src/lib.rs

---
+++ leetcode-palindrome-number/src/lib.rs
@@ -0,0 +1,41 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        let mut temp_x = x;
+        if x < 0 {
+            false
+        } else {
+            let mut swapped_num = 0;
+            while temp_x > 0 {
+                let num_unit = temp_x % 10;
+                temp_x /= 10;
+                swapped_num = swapped_num * 10 + num_unit;
+            }
+            swapped_num == x
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = Solution::is_palindrome(101);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn it_does_not_work_with_negative() {
+        let result = Solution::is_palindrome(-101);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn it_does_not_work() {
+        let result = Solution::is_palindrome(10);
+        assert_eq!(result, false);
+    }
+}
